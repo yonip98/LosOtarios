112895e20cb55e39717a800cdb52c81444f04867
pineapplepicsa <pineapplepizza@vivaldi.net>
2019-06-02 21:02:01 -0300

    El ejercicio 5 ahora cumple su función
    Se le hicieron algunos comentarios al código para que se entiendan los procesos

--- C++/C17-18/Practica/Ejercicio 05 pruebas.cpp
+++ C++/C17-18/Practica/Ejercicio 05 pruebas.cpp
@@ -5,19 +5,36 @@ using namespace std;
 int main ()
 {
 
-  int A[8], i;
+	int A[5], i, u, b;
 
-  for (i=0; i<8; i++)
-  {
-    cout<<"Asigne un valor: ";
-    cin>>A[i];
-  }
+  //En esta primera instancia se ingresan los valores del vector
+  //Donde "i" es la posición del valor dentro del vector
+	for (i=1; i<6; i++)
+	{
+    //Aquí se solicita que se ingrese un valor en una posición específica
+		cout<<"Asigne un valor en la posición "<<i<<": ";
+		cin>>A[i];
+	}
+	b = 0; //La variable b servirá más adelante para saber si ningun valor se repite
+	for (i=1; i<6; i++)
+	{
+		for (u=i+1; u<6; u++)
+		{
+			if (A[i] == A[u])
+			{
+				cout<<"El valor en la posición "<<i<<" se repite con el de la posición "<<u<<endl;
+			}
+			else
+			{
+				b++;
+				if (b == 10)
+				{
+					cout<<"Ningun valor se repite";
+				}
+			}
+		}
+	}
 
-  for (i=0; i<8; i++)
-  {
-    cout<<A[i];
-  }
-
-  return(0);
+	return(0);
 
 }
