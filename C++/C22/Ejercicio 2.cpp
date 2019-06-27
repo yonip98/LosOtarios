551a7982f04852dcf392bae01dce05b83d640dda
Gabriel Da Silva <50386282+FloppRap@users.noreply.github.com>
2019-06-27 09:48:51 -0300

    Create Ejercicio 2.cpp

---
+++ C++/C22/Ejercicio 2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#define N 3
+using namespace std;
+
+int main() {
+	
+	/*Escribir un programa que cargue una matriz cuadrada y muestre la suma de
+	los elementos de la diagonal principal. Mostrar la matriz.*/
+	
+	int vec[N][N],i,j,suma=0;
+	
+	for(i=0;i<N;i++)
+	{
+		for(j=0;j<N;j++)
+		{
+			cout<<"Ingrese el elemento ["<<i<<"]["<<j<<"]: ";
+			cin>>vec[i][j];
+		}
+		cout << endl;
+	}
+	for(i=0;i<N;i++)
+	{
+		for(j=0;j<N;j++)
+		{
+			if(j==((N-1)-i))
+			{
+				cout<<vec[i][j]<<" ";
+				suma=suma+(vec[i][j]);
+			}
+			else 
+			   cout<<"- ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
